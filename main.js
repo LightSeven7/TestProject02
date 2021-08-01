ee81e66192b663709a560175a8b441f7680c7ceb
LightSeven7 <light_seven_k@yahoo.co.jp>
2021-08-01 23:06:34 +0900

    mod file

--- main.js
+++ main.js
@@ -856,9 +856,17 @@ const TestBind = () => {
 }
 
 function TestSample () {
+
     var sum = 0;
-    [1,2,3,4,5,6,7,8,9,10].map(function (value) {
-        value % 2 === 0 ? sum += value * 2 : sum += value;
-    });
-    alert('SUM:' + sum);
+    var array_num = [1,2,3,4,5,6,7,8,9,10];
+
+    // ES5までの書き方
+    // array_num.map(function (value) {
+    //     value % 2 === 0 ? sum += value * 2 : sum += value;
+    // });
+
+    // ES6以降の書き方
+    array_num.map((value) => value % 2 === 0 ? sum += value * 2 : sum += value);
+
+    return alert('SUM:' + sum);
 }
